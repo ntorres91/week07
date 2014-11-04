5813872634b79e18af0f5c8293ba811c53ace634
ntorres91 <neil.k.torres@gmail.com>
2014-11-04 17:23:08 -0500

    added rock_paper_scissor class and rules to game

---
+++ rps/rock_paper_scissors.rb
@@ -0,0 +1,12 @@
+class RockPaperScissors
+  BEATS = {
+    "R" => "S",
+    "P" => "R",
+    "S" => "P"
+  }
+
+  def initialize
+    @score = []
+  end
+
+end 
---
+++ rps/test.rb
@@ -0,0 +1,7 @@
+class RockPaperScissors
+  puts BEATS = {
+    "R" => "S",
+    "P" => "R",
+    "S" => "P"
+  }
+end 
