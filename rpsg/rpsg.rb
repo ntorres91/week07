34c53b0df688b48b3785bb516ee2182d8304f24a
ntorres91 <neil.k.torres@gmail.com>
2014-11-07 10:01:08 -0500

    fixed logic within class

--- rpsg/rpsg.rb
+++ rpsg/rpsg.rb
@@ -10,26 +10,37 @@ class RockPaperScissors
   puts "Welcome to Rock, Paper, Scissors"
   puts "What is your choice: Rock, Paper, or Scissors? "
   player_choice = gets.chomp.upcase 
-  ai_choice = 
+  ai = Ai.new
+  ai_choice = ai.choose
     
-      if player_choice == ai_choice[0]
+      if player_choice == ai_choice
         puts "It's a Tie"
-      elsif player_choice == "R" && ["S","P"].include?(ai_choice[0])
-        puts "You Win, Rock beats #{ai_choice[1]}"
-      elsif (player_choice == "S" && ai_choice[0] == "P")
-        puts "You Win, Scissors beats #{ai_choice[1]}"
-      elsif (player_choice == "P" && ai_choice[0] == ("S"))
-        puts "You Lose, Paper loses to #{ai_choice[1]}"
-      elsif (player_choice == "P" && ai_choice[0] == ("R"))
-        puts "You Win, Paper beats #{ai_choice[1]}"
-      elsif (player_choice == "S" && ai_choice[0] == "R")
-        puts "You Lose, Scissors loses to #{ai_choice[1]}"
+      elsif player_choice == "R" && ["S","P"].include?(ai_choice)
+        puts "You Win, Rock beats #{ai_choice}"
+      elsif (player_choice == "S" && ai_choice == "P")
+        puts "You Win, Scissors beats #{ai_choice}"
+      elsif (player_choice == "P" && ai_choice == ("S"))
+        puts "You Lose, Paper loses to #{ai_choice}"
+      elsif (player_choice == "P" && ai_choice == ("R"))
+        puts "You Win, Paper beats #{ai_choice}"
+      elsif (player_choice == "S" && ai_choice == "R")
+        puts "You Lose, Scissors loses to #{ai_choice}"
       end
-      puts ai_choice[0]
+      puts ai_choice
       puts player_choice
   
   end
 end
+
+class Ai
+  def choose
+    options=[]
+    "R"
+  end 
+
+end 
+
+
 
 
 game = RockPaperScissors.new
